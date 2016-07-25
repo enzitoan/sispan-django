bcb1c6b340a10f4722a57328bec678846203c402
enzitoan <ahumada.enzo@gmail.com>
2016-07-24 22:35:09 -0400

    prueba css 3

--- wsgi/app/app/settings.py
+++ wsgi/app/app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vf#oh=b00n4nohoriwsv3v4f5pya5xd10*c23_=#ucns8vngu8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
