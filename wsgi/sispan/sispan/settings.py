e69cd26e1d54e382cb62fc0056fa657be869dd6a
enzitoan <ahumada.enzo@gmail.com>
2016-07-26 11:18:30 -0400

    test

--- wsgi/sispan/sispan/settings.py
+++ wsgi/sispan/sispan/settings.py
@@ -111,8 +111,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(WSGI_DIR, 'static')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'recursos'),
-]
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'wsgi', 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'wsgi', 'static', 'media')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'sispan', 'static'),)
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'sispan', 'templates'),)
