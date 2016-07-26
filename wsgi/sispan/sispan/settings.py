f618976c4c9614f8a4d0b17c79350be5041fdd0b
enzitoan <ahumada.enzo@gmail.com>
2016-07-26 14:58:43 -0400

    Rollback secret key

--- wsgi/sispan/sispan/settings.py
+++ wsgi/sispan/sispan/settings.py
@@ -16,18 +16,19 @@ DJ_PROJECT_DIR = os.path.dirname(__file__)
 BASE_DIR = os.path.dirname(DJ_PROJECT_DIR)
 WSGI_DIR = os.path.dirname(BASE_DIR)
 REPO_DIR = os.path.dirname(WSGI_DIR)
-DATA_DIR = os.environ.get($OPENSHIFT_DATA_DIR, BASE_DIR)
+DATA_DIR = os.environ.get('OPENSHIFT_DATA_DIR', BASE_DIR)
 
-import sys
-sys.path.append(os.path.join(REPO_DIR, 'libs'))
-import secrets
-SECRETS = secrets.getter(os.path.join(DATA_DIR, 'secrets.json'))
+#import sys
+#sys.path.append(os.path.join(REPO_DIR, 'libs'))
+#import secrets
+#SECRETS = secrets.getter(os.path.join(DATA_DIR, 'secrets.json'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRETS['secret_key']
+#SECRET_KEY = SECRETS['secret_key']
+SECRET_KEY = 'ynur=j=(ggeppoeux5+j2i-*@u+43_#!isfv-cw%f*d9-*1&tz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
@@ -35,7 +36,7 @@ DEBUG = os.environ.get('DEBUG') == 'True'
 from socket import gethostname
 ALLOWED_HOSTS = [
     gethostname(), # For internal OpenShift load balancer security purposes.
-    os.environ.get($OPENSHIFT_APP_DNS), # Dynamically map to the OpenShift gear name.
+    os.environ.get('OPENSHIFT_APP_DNS'), # Dynamically map to the OpenShift gear name.
     #'example.com', # First DNS alias (set up in the app)
     #'www.example.com', # Second DNS alias (set up in the app)
 ]
