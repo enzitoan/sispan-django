6028becf41052de0e973dc3cd5d6527112f7f628
enzitoan <ahumada.enzo@gmail.com>
2016-07-26 16:07:17 -0400

    fix test submit

--- wsgi/sispan/pedido/models.py
+++ wsgi/sispan/pedido/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from datetime import datetime, date
+from datetime import date
+import datetime
 
 # Create your models here.
 class Pan(models.Model):
@@ -15,7 +16,7 @@ class Detalle(models.Model):
         if days_ahead <= 0: # Target day already happened this week
             days_ahead += 7
         return d + datetime.timedelta(days_ahead)
-        
+
     pedido = date.today
     entrega = next_weekday(date.today, 1)
 
