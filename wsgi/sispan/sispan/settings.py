49aa42065d7c72fd816581ced272a1547658a0a1
enzitoan <ahumada.enzo@gmail.com>
2016-07-26 10:56:46 -0400

    cambios setting

--- wsgi/sispan/sispan/settings.py
+++ wsgi/sispan/sispan/settings.py
@@ -20,9 +20,6 @@ DATA_DIR = os.environ.get('OPENSHIFT_DATA_DIR', BASE_DIR)
 
 import sys
 sys.path.append(os.path.join(REPO_DIR, 'libs'))
-import secrets
-SECRETS = secrets.getter(os.path.join(DATA_DIR, 'secrets.json'))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
