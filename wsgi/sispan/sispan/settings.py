faf0b4dfd8476b5b9b70b5364f14f807749a0157
enzitoan <ahumada.enzo@gmail.com>
2016-07-26 15:24:42 -0400

    Camba zona horaria y lenguaje

--- wsgi/sispan/sispan/settings.py
+++ wsgi/sispan/sispan/settings.py
@@ -99,9 +99,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-CL'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
