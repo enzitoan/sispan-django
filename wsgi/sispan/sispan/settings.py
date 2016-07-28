158f1b3b6e0e00172102e100a74551bdd000ac0b
enzitoan <ahumada.enzo@gmail.com>
2016-07-28 16:52:56 -0400

    modifica formato log

--- wsgi/sispan/sispan/settings.py
+++ wsgi/sispan/sispan/settings.py
@@ -143,7 +143,8 @@ LOGGING = {
     'disable_existing_loggers': True,
     'formatters': {
         'standard': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            #'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'format' : "[SISPAN] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
             'datefmt' : "%d/%b/%Y %H:%M:%S"
         },
     },
