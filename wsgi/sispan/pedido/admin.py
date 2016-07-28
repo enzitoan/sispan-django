8fd4d2f4a5d32de6cb5122731d54044e8660f20e
enzitoan <ahumada.enzo@gmail.com>
2016-07-28 17:42:05 -0400

    modifica DetalleAdmin

--- wsgi/sispan/pedido/admin.py
+++ wsgi/sispan/pedido/admin.py
@@ -14,6 +14,15 @@ class DetalleAdmin(admin.ModelAdmin):
 		'cantidad_amasado_blanco',
 	]
 
+	list_filter = ('fecha_entrega')
+	
+	fields = (
+		'cantidad_pita_integral',
+		'cantidad_pita_blanco', 
+		'cantidad_amasado_integral',
+		'cantidad_amasado_blanco',
+	)
+
 class PanAdmin(admin.ModelAdmin):
 	list_display =  [
 		'codigo',
