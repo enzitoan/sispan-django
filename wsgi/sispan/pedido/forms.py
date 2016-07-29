bee54287916a0090ec7f7d85a13ef9c2d4171df8
enzitoan <ahumada.enzo@gmail.com>
2016-07-29 11:25:23 -0400

    change forms.py en eclipse

--- wsgi/sispan/pedido/forms.py
+++ wsgi/sispan/pedido/forms.py
@@ -4,76 +4,76 @@ from .models import Detalle
 
 class DetalleForm(forms.ModelForm):
 
-	class Meta:
-		model = Detalle
+    class Meta:
+        model = Detalle
 
-		fields = [
-		    'cantidad_pita_blanco',
-		    'cantidad_pita_integral',
-		    'cantidad_amasado_blanco',
-		    'cantidad_amasado_integral',
-		    'nombre',
-		    'email',
-		]
-		labels = {
-		    'cantidad_pita_blanco': 'Pita Blanco',
-		    'cantidad_pita_integral': 'Pita Integral',
-		    'cantidad_amasado_blanco': 'Amasado Blanco',
-		    'cantidad_amasado_integral': 'Amasado Integral',
-		    'nombre': 'Nombre Completo',
-		    'email': 'E-Mail',		
-		}
-		widgets = {
-			'cantidad_pita_blanco': forms.NumberInput(
-				attrs={
-					'id': 'txtPan1',
-					'class':'form-control txt-pedido',
-					'min': 1,
-					'required': True,
-					'disabled': True,
-				}
-			),
-			'cantidad_pita_integral': forms.NumberInput(
-				attrs={
-					'id': 'txtPan2',
-					'class':'form-control txt-pedido',
-					'min': 1,
-					'required': True,
-					'disabled': True,
-				}
-			),
-			'cantidad_amasado_blanco': forms.NumberInput(
-				attrs={
-					'id': 'txtPan3',
-					'class':'form-control txt-pedido',
-					'min': 1,
-					'required': True,
-					'disabled': True,
-				}
-			),
-			'cantidad_amasado_integral': forms.NumberInput(
-				attrs={
-					'id': 'txtPan4', 
-					'class':'form-control txt-pedido',
-					'min': 1,
-					'required': True,
-					'disabled': True,
-				}
-			),
-			'nombre': forms.TextInput(
-				attrs={
-					'id': 'txtNombre',
-					'class':'form-control',
-					'required': True,					
-					'disabled': True,
-				}
-			),
-			'email': forms.EmailInput(
-				attrs={
-					'id': 'txtCorreo',
-					'class':'form-control',
-					'required': True,
-					'disabled': True,
-				}
-			),
-		}
+        fields = [
+            'cantidad_pita_blanco',
+            'cantidad_pita_integral',
+            'cantidad_amasado_blanco',
+            'cantidad_amasado_integral',
+            'nombre',
+            'email',
+        ]
+        labels = {
+            'cantidad_pita_blanco': 'Pita Blanco',
+            'cantidad_pita_integral': 'Pita Integral',
+            'cantidad_amasado_blanco': 'Amasado Blanco',
+            'cantidad_amasado_integral': 'Amasado Integral',
+            'nombre': 'Nombre Completo',
+            'email': 'E-Mail',        
+        }
+        widgets = {
+            'cantidad_pita_blanco': forms.NumberInput(
+                attrs={
+                    'id': 'txtPan1',
+                    'class':'form-control txt-pedido',
+                    'min': 1,
+                    'required': True,
+                    'disabled': True,
+                }
+            ),
+            'cantidad_pita_integral': forms.NumberInput(
+                attrs={
+                    'id': 'txtPan2',
+                    'class':'form-control txt-pedido',
+                    'min': 1,
+                    'required': True,
+                    'disabled': True,
+                }
+            ),
+            'cantidad_amasado_blanco': forms.NumberInput(
+                attrs={
+                    'id': 'txtPan3',
+                    'class':'form-control txt-pedido',
+                    'min': 1,
+                    'required': True,
+                    'disabled': True,
+                }
+            ),
+            'cantidad_amasado_integral': forms.NumberInput(
+                attrs={
+                    'id': 'txtPan4', 
+                    'class':'form-control txt-pedido',
+                    'min': 1,
+                    'required': True,
+                    'disabled': True,
+                }
+            ),
+            'nombre': forms.TextInput(
+                attrs={
+                    'id': 'txtNombre',
+                    'class':'form-control',
+                    'required': True,                    
+                    'disabled': True,
+                }
+            ),
+            'email': forms.EmailInput(
+                attrs={
+                    'id': 'txtCorreo',
+                    'class':'form-control',
+                    'required': True,
+                    'disabled': True,
+                }
+            ),
+        }
